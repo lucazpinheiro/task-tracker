63a6b18ea4b38fb2d348b787cbfc3d1bf740e77e
lucas <lucascpinheiro10@gmail.com>
2020-07-14 16:30:12 -0300

    remove unnecessary file

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,11 +6,22 @@ import {
   DoneColumn,
   ReviewColumn,
 } from './columns/index';
-import Controller from './Controller';
 import '../App.css';
 
+function selectIssues(issuesList) {
+  const todoList = issuesList.filter((issue) => issue.status === 'to-do');
+
+  const doingList = issuesList.filter((issue) => issue.status === 'doing');
+
+  const reviewList = issuesList.filter((issue) => issue.status === 'review');
+
+  const doneList = issuesList.filter((issue) => issue.status === 'done');
+
+  return [todoList, doingList, reviewList, doneList];
+}
+
 export default function Board({ issues }) {
-  const [todoList, doingList, reviewList, doneList] = Controller(issues);
+  const [todoList, doingList, reviewList, doneList] = selectIssues(issues);
   return (
     <div className="board">
       <BackLogColumn content={todoList} />
