50c0786391215127d762ec372995a4e681d90f29
lucas <lucascpinheiro10@gmail.com>
2020-07-14 12:19:22 -0300

    fix eslint complain

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,19 +1,11 @@
 export default function Controller(issuesList) {
-  const todoList = issuesList.filter((issue) => {
-    if (issue.status === 'to-do') return issue;
-  });
+  const todoList = issuesList.filter((issue) => issue.status === 'to-do');
 
-  const doingList = issuesList.filter((issue) => {
-    if (issue.status === 'doing') return issue;
-  });
+  const doingList = issuesList.filter((issue) => issue.status === 'doing');
 
-  const reviewList = issuesList.filter((issue) => {
-    if (issue.status === 'review') return issue;
-  });
+  const reviewList = issuesList.filter((issue) => issue.status === 'review');
 
-  const doneList = issuesList.filter((issue) => {
-    if (issue.status === 'done') return issue;
-  });
+  const doneList = issuesList.filter((issue) => issue.status === 'done');
 
   return [todoList, doingList, reviewList, doneList];
 }
