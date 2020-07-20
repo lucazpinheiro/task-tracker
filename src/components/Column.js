8fb48846927f1b8309018fccebf9ea943cc30496
lucas <lucascpinheiro10@gmail.com>
2020-07-20 10:15:14 -0300

    refactor column component

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
-import '../../App.css';
+import '../App.css';
 
 function BackLogColumn({ content, updateStatus }) {
   const TITLE = 'to-do';
